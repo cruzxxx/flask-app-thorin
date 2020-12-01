15774560bc047606034e2b89a8198d830425c135
stefcruz <crzstefanie@gmail.com>
2020-12-01 08:05:36 +0000

    contact form, member page

--- run.py
+++ run.py
@@ -1,9 +1,12 @@
 import os
-from flask import Flask, render_template
+import json
+from flask import Flask, render_template, request, flash
+if os.path.exists("env.py"):
+    import env
 
 # Create an instance of the flask class
 app = Flask(__name__)
-
+app.secret_key = os.environ.get("SECRET_KEY")
 
 @app.route("/")
 def index():
@@ -12,17 +15,33 @@ def index():
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    data = []
+    with open("data/company.json", "r") as json_data:
+        data = json.load(json_data)
+    return render_template("about.html", page_title="About", company=data)
 
 
-@app.route("/contact")
+@app.route("/about/<member_name>")
+def about_member(member_name):
+    with open("data/company.json", "r") as json_data:
+        data = json.load(json_data)
+        for obj in data:
+            if obj["url"] == member_name:
+                member = obj
+    return render_template("member.html", member=member)
+
+
+@app.route("/contact", methods=["GET", "POST"])
 def contact():
-    return render_template("contact.html")
+    if request.method == "POST":
+        flash("Thanks {}, we have received your message".format(
+            request.form.get("name")))
+    return render_template("contact.html", page_title="Contact")
 
 
 @app.route("/careers")
 def careers():
-    return render_template("careers.html")
+    return render_template("careers.html", page_title="Careers")
 
 
 if __name__ == "__main__":
@@ -32,4 +51,5 @@ if __name__ == "__main__":
         debug=True
     )
 
-# debug=True only for test environment, when submitting project change to debug=False.
+# debug=True only for test environment,
+# when submitting project change to debug=False.
